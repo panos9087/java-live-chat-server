4b5e473ca50b81c3981f529e15c8b8e8839284b5
student-programmer-panos <studentprogrammer0@gmail.com>
2025-11-10 14:17:43 +0200

    first commit of java server

---
+++ src/live/chat/server/HandleClient.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package live.chat.server;
+
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author panos
+ */
+public class HandleClient implements Runnable {
+
+    private ArrayList<Socket> clients;
+    private Socket currentClient;
+    private InputStream clientInput;
+    private OutputStream clientOutput;
+
+    public HandleClient(Socket clientSocket, ArrayList<Socket> clientList) throws IOException {
+        //this.clients = new ArrayList<Socket>();
+        this.clients = clientList;
+        this.currentClient = clientSocket;
+        //clientSocket.getInputStream()
+        this.clientInput = currentClient.getInputStream();
+        this.clientOutput = currentClient.getOutputStream();
+
+    }
+
+    public void run() {
+        try {
+            String line = "";
+            int data = 0;
+            while (!this.currentClient.isClosed()) {
+                System.out.println("Waiting for data...");
+                while ((data = clientInput.read()) != 4) {
+                    System.out.println(data);
+                    
+                    line += (char) data;
+                }
+                data=0;
+                System.out.println("got msg : " + line + " clients size : " + clients.size());
+                if (line.equals("\\bye")) {
+                    this.currentClient.close();
+                    this.clientInput.close();
+                    this.clientOutput.close();
+                    System.out.println("did client removed from the clients list = " + clients.remove(currentClient));
+                } else {
+                    for (Socket client : clients) {
+                        OutputStream clientOut = client.getOutputStream();
+                        //char[] message = line.toCharArray();
+                        byte[] bytes = line.getBytes();
+                        clientOut.write(bytes);
+                        clientOut.write(4);
+                    }
+                    line = "";
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Thread has reached the end of it's execution for client: " + this.currentClient.getInetAddress().toString());
+    }
+
+    public ArrayList<Socket> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<Socket> clients) {
+        this.clients = clients;
+    }
+
+}
---
+++ src/live/chat/server/JavaLiveChatServer.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package live.chat.server;
+
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author panos
+ */
+public class JavaLiveChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        ArrayList<Socket> clients = new ArrayList<Socket>(10);
+        ServerSocket server = new ServerSocket(65001);
+
+        while (true) {
+            if (clients.size() <= 10) {
+                Socket client = server.accept();
+                clients.add(client);
+                HandleClient clientHandler = new HandleClient(client, clients);
+                Thread runClient = new Thread(clientHandler);
+                runClient.start();
+                System.out.println("new client connected @"+client.getPort());
+            } else {
+                try {
+                    System.out.println("Server has reached maximum capacity of " + clients.size() + " clients and won't accept any new clients");
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
+
+}
